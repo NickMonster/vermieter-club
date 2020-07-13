7c94d994e07cb57f77d2d5fed5c3be04b1eeec12
Mykola Kushka <Mykola.Kushka@gmail.com>
2020-07-14 01:42:19 +0300

    Feedback changes

--- js/common.js
+++ js/common.js
@@ -6,16 +6,22 @@ form.addEventListener(
   'change',
   evt => {
     let trg = evt.target,
-      trg_par = trg.closest('.plan__item');
+      trg_par = trg.closest('.plan__item'),
+      trg_button = trg.nextSibling.nextSibling;
 
     if (trg.type === 'radio' && trg_par) {
       let prior = form.querySelector('.plan__item.checked');
 
       if (prior) {
+        let trg_button = prior.querySelector(
+          '.plan__item.checked .btn__orange',
+        );
+        trg_button.innerText = 'Auswählen';
         prior.classList.remove('checked');
       }
 
       trg_par.classList.add('checked');
+      trg_button.innerText = 'Ihr Plan';
     }
   },
   false,
