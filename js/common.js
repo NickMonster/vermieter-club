93372c487ee28588639a038d11c6877050677c13
Mykola Kushka <Mykola.Kushka@gmail.com>
2020-08-14 19:35:57 +0300

    Custom input field, landing updates

--- js/common.js
+++ js/common.js
@@ -101,3 +101,25 @@ Array.from(selector__items).forEach(el2 => {
     }
   });
 });
+
+// Custom file field
+var inputs = document.querySelectorAll('.file__custom');
+Array.prototype.forEach.call(inputs, function (input) {
+  var label = input.nextElementSibling,
+    labelVal = label.innerHTML;
+
+  input.addEventListener('change', function (e) {
+    var fileName = '';
+    if (this.files && this.files.length > 1) {
+      fileName = (this.getAttribute('data-multiple-caption') || '').replace(
+        '{count}',
+        this.files.length,
+      );
+    } else {
+      fileName = e.target.value.split('\\').pop();
+    }
+
+    if (fileName) label.querySelector('span').innerHTML = fileName;
+    else label.innerHTML = labelVal;
+  });
+});
